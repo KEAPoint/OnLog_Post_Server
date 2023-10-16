d85696b0403cb899cb4bcb242e15cac7d8ec0f98
SeungHyeon <namsh1125@naver.com>
2023-10-17 01:15:14 +0900

    [Feat] Blog 생성 API 개발

--- src/main/java/keapoint/onlog/post/controller/BlogController.java
+++ src/main/java/keapoint/onlog/post/controller/BlogController.java
@@ -1,9 +1,30 @@
 package keapoint.onlog.post.controller;
 
+import keapoint.onlog.post.base.BaseException;
+import keapoint.onlog.post.base.BaseResponse;
+import keapoint.onlog.post.dto.post.PostCreateBlogReqDto;
+import keapoint.onlog.post.dto.post.PostCreateBlogResDto;
+import keapoint.onlog.post.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/blog")
 public class BlogController {
+
+    private final BlogService blogService;
+
+    @PostMapping("")
+    public BaseResponse<PostCreateBlogResDto> createBlog(@RequestBody PostCreateBlogReqDto data) {
+        try {
+            return new BaseResponse<>(blogService.createBlog(data));
+
+        } catch (BaseException exception) {
+            return new BaseResponse<>(exception);
+        }
+    }
 }
---
+++ src/main/java/keapoint/onlog/post/dto/post/PostCreateBlogReqDto.java
@@ -0,0 +1,23 @@
+package keapoint.onlog.post.dto.post;
+
+import keapoint.onlog.post.entity.Blog;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class PostCreateBlogReqDto {
+    private UUID blogId; // 사용자 블로그 id
+    private String blogName; // 사용자 블로그 이름
+    private String blogNickname; // 사용자 블로그 별명
+    private String blogIntro; // 사용자 블로그 한 줄 소개
+
+    public Blog toEntity() {
+        return Blog.builder()
+                .blogId(blogId)
+                .blogName(blogName)
+                .blogNickname(blogNickname)
+                .blogIntro(blogIntro)
+                .build();
+    }
+}
