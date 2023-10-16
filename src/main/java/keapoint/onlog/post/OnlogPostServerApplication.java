10b01a92581c2dba254269d25ab292bef7076107
SeungHyeon <namsh1125@naver.com>
2023-10-16 09:26:23 +0900

    Post server initial commit

---
+++ src/main/java/keapoint/onlog/post/OnlogPostServerApplication.java
@@ -0,0 +1,13 @@
+package keapoint.onlog.post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlogPostServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlogPostServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/keapoint/onlog/post/OnlogPostServerApplicationTests.java
@@ -0,0 +1,13 @@
+package keapoint.onlog.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlogPostServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
