8b69b0f542e96ce10d6a3514d41c393abf281c37
SeungHyeon <namsh1125@naver.com>
2023-10-19 22:41:52 +0900

    [Delete] 불필요한 파일 제거

--- src/main/java/keapoint/onlog/post/repository/PostHashtagRepository.java
+++
@@ -1,8 +0,0 @@
-package keapoint.onlog.post.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostHashtagRepository extends JpaRepository<PostHashtag, Long> {
-}
