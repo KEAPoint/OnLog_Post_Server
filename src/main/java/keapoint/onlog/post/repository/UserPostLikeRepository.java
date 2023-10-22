f57d3984b8aea3e5daa4f849069521ba1f0f4cfd
SeungHyeon <namsh1125@naver.com>
2023-10-22 20:11:46 +0900

    [Feat] 게시글 좋아요 변경 API 개발

--- src/main/java/keapoint/onlog/post/repository/UserPostLikeRepository.java
+++ src/main/java/keapoint/onlog/post/repository/UserPostLikeRepository.java
@@ -1,10 +1,15 @@
 package keapoint.onlog.post.repository;
 
+import keapoint.onlog.post.entity.Blog;
+import keapoint.onlog.post.entity.Post;
 import keapoint.onlog.post.entity.UserPostLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserPostLikeRepository extends JpaRepository<UserPostLike, Long> {
+    Optional<UserPostLike> findByBlogAndPost(Blog blog, Post post);
 }
 
---
+++ src/main/java/keapoint/onlog/post/utils/JwtTokenProvider.java
@@ -0,0 +1,139 @@
+package keapoint.onlog.post.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import keapoint.onlog.post.base.BaseErrorCode;
+import keapoint.onlog.post.base.BaseException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtTokenProvider {
+    @Value("${jwt.secret-key}")
+    private String jwtKey;
+
+    private final AES256 aes256;
+
+    @Autowired
+    public JwtTokenProvider(AES256 aes256) {
+        this.aes256 = aes256;
+    }
+
+    /**
+     * 사용자의 식별자를 추출하는 method
+     *
+     * @param token 사용자 token
+     * @return 사용자 식별자
+     */
+    public String extractIdx(String token) throws BaseException {
+        try {
+            Jws<Claims> claimsJws = Jwts.parserBuilder()
+                    .setSigningKey(jwtKey.getBytes())
+                    .build()
+                    .parseClaimsJws(token);
+
+            return claimsJws.getBody().get("memberIdx", String.class);
+
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new BaseException(BaseErrorCode.INVALID_TOKEN_EXCEPTION);
+        }
+    }
+
+
+    /**
+     * access token에서 사용자의 정보 추출
+     *
+     * @param token access token
+     * @return UsernamePasswordAuthenticationToken
+     */
+    public Authentication getAuthentication(String token) throws BaseException {
+        Jws<Claims> claimsJws;
+
+        try {
+            claimsJws = Jwts.parserBuilder()
+                    .setSigningKey(jwtKey.getBytes()).build()
+                    .parseClaimsJws(token);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            log.error(exception.getMessage());
+            throw new BaseException(BaseErrorCode.TOKEN_DECODING_EXCEPTION);
+        }
+
+        // 인증 정보 받아오기
+        Collection<? extends GrantedAuthority> authorities =
+                // Jwt token 에서 auth 필드에대한 스트림을 열어서 ,로 split 한다음에 SimpleGrantedAuthority 로 매핑해서 리스트로 반환
+                Arrays.stream(claimsJws.getBody().get("auth").toString().split(","))
+                        .map(SimpleGrantedAuthority::new).toList();
+
+        UserDetails principal = new User(claimsJws.getBody().getSubject(), "", authorities);
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
+    }
+
+    /**
+     * access token 유효성 검사
+     *
+     * @param token access token
+     * @return boolean (true: valid, false: invalid)
+     */
+    public boolean vallidateToken(String token) {
+        Key key = Keys.hmacShaKeyFor(jwtKey.getBytes());
+
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception exception) {
+            log.info("invalid JWT Token" + exception.getMessage());
+        }
+
+        return false;
+    }
+
+    /**
+     * refresh token 유효성 검사
+     *
+     * @param refreshToken
+     * @return
+     * @throws BaseException
+     */
+    public String vallidateRefreshToken(String refreshToken) throws BaseException {
+        Jws<Claims> claims;
+
+        try {
+            log.info(String.valueOf(refreshToken));
+            claims = Jwts.parserBuilder()
+                    .setSigningKey(jwtKey.getBytes())
+                    .build()
+                    .parseClaimsJws(refreshToken);
+
+            Date expired = claims.getBody().getExpiration();
+            Date now = new Date();
+
+            if (!expired.after(now)) throw new RuntimeException();
+
+            return aes256.decrypt(claims.getBody().get("password").toString());
+
+        } catch (Exception exception) {
+            log.error(exception.getMessage());
+            throw new BaseException(BaseErrorCode.TOKEN_EXPIRED_EXCEPTION);
+        }
+    }
+
+}
