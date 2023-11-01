36927da04926a2803bccbeed6e67d49ed528e194
namsh1125 <namsh1125@naver.com>
2023-11-01 21:29:55 +0900

    [Fix] Blog 생성 요청 data 변경 및 응답 data 변경

--- src/main/java/keapoint/onlog/post/dto/blog/PostCreateBlogReqDto.java
+++ src/main/java/keapoint/onlog/post/dto/blog/PostCreateBlogReqDto.java
@@ -9,17 +9,18 @@ import java.util.UUID;
 @Data
 @Builder
 public class PostCreateBlogReqDto {
-    private UUID blogId; // 사용자 블로그 id
     private String blogName; // 사용자 블로그 이름
     private String blogNickname; // 사용자 블로그 별명
     private String blogIntro; // 사용자 블로그 한 줄 소개
+    private String blogProfileImg; // 사용자 블로그 프로필
 
-    public Blog toEntity() {
+    public Blog toEntity(UUID blogId) {
         return Blog.builder()
                 .blogId(blogId)
                 .blogName(blogName)
                 .blogNickname(blogNickname)
                 .blogIntro(blogIntro)
+                .blogProfileImg(blogProfileImg)
                 .build();
     }
 }
--- src/test/java/keapoint/onlog/post/service/BlogServiceUnitTest.java
+++ src/test/java/keapoint/onlog/post/service/BlogServiceUnitTest.java
@@ -33,75 +33,79 @@ class BlogServiceUnitTest {
     @Test
     void 블로그_생성() throws BaseException {
         // given: 새로운 블로그 생성이 들어왔을 때
+        UUID blogId = UUID.fromString("48f99c85-ed6b-46c2-8f47-66f9f67040bc");
         PostCreateBlogReqDto reqDto = PostCreateBlogReqDto.builder()
-                .blogId(UUID.fromString("48f99c85-ed6b-46c2-8f47-66f9f67040bc")) // 사용자 블로그 id
                 .blogName("Hani Tech World") // 사용자 블로그 이름
                 .blogNickname("hanitech") // 사용자 블로그 별명. 닉네임
                 .blogIntro("Hani Tech World는 최신 기술 정보와 실용적인 IT 팁을 제공하는 블로그입니다.") // 사용자 블로그 한 줄 소개
+                .blogProfileImg("http://haniTechProfileImageUrl.com") // 사용자 프로필 이미지
                 .build();
 
         // when: 별 이상이 없는 경우 (중복된 식별자가 사용되지 않는 경우, 닉네임 중복이 없는 경우)
-        blogService.createBlog(reqDto);
+        blogService.createBlog(blogId, reqDto);
 
         // then: 블로그가 생성되어야 한다.
         Blog findBlog = blogRepository.findByBlogNickname("hanitech").get();
 
         // verify: 블로그 생성 요청 식별자와 생성된 블로그의 식별자가 같은지 검증
-        assertEquals(reqDto.getBlogId(), findBlog.getBlogId());
+        assertEquals(blogId, findBlog.getBlogId());
     }
 
     @Test
     void 블로그_생성_2() throws BaseException {
         // given: 새로운 블로그 생성이 들어왔을 때
+        UUID blogId = UUID.fromString("48f99c85-ed6b-46c2-8f47-66f9f67040bc");
         PostCreateBlogReqDto reqDto = PostCreateBlogReqDto.builder()
-                .blogId(UUID.fromString("48f99c85-ed6b-46c2-8f47-66f9f67040bc")) // 사용자 블로그 id
                 .blogName("Hani Tech World") // 사용자 블로그 이름
                 .blogNickname("hanitech") // 사용자 블로그 별명. 닉네임
                 .blogIntro("Hani Tech World는 최신 기술 정보와 실용적인 IT 팁을 제공하는 블로그입니다.") // 사용자 블로그 한 줄 소개
+                .blogProfileImg("http://haniTechProfileImageUrl.com") // 사용자 프로필 이미지
                 .build();
 
-        blogService.createBlog(reqDto);
+        blogService.createBlog(blogId, reqDto);
 
+        UUID anotherBlogId = UUID.fromString("5a7a63d7-02c7-43a0-82ec-f2a80bd2eba4");
         PostCreateBlogReqDto newReqDto = PostCreateBlogReqDto.builder()
-                .blogId(UUID.fromString("5a7a63d7-02c7-43a0-82ec-f2a80bd2eba4")) // 사용자 블로그 id
                 .blogName("Wooseok Tech World") // 사용자 블로그 이름
                 .blogNickname("wooseoktech") // 사용자 블로그 별명. 닉네임
                 .blogIntro("Woo Seok Tech World는 최신 기술 정보와 실용적인 IT 팁을 제공하는 블로그입니다.") // 사용자 블로그 한 줄 소개
+                .blogProfileImg("http://wooseokTechProfileImageUrl.com") // 사용자 프로필 이미지
                 .build();
 
         // when: 별 이상이 없는 경우 (중복된 식별자가 사용되지 않는 경우, 닉네임 중복이 없는 경우)
-        blogService.createBlog(newReqDto);
+        blogService.createBlog(anotherBlogId, newReqDto);
 
         // then: 블로그가 생성되어야 한다.
         Blog findBlog = blogRepository.findByBlogNickname("wooseoktech").get();
 
         // verify: 블로그 생성 요청 식별자와 생성된 블로그의 식별자가 같은지 검증
-        assertEquals(newReqDto.getBlogId(), findBlog.getBlogId());
+        assertEquals(anotherBlogId, findBlog.getBlogId());
     }
 
     @Test
     void 블로그_생성_실패_식별자_중복() throws BaseException {
         // given: 사전에 있는 블로그 식별자로
+        UUID blogId = UUID.fromString("48f99c85-ed6b-46c2-8f47-66f9f67040bc");
         PostCreateBlogReqDto reqDto = PostCreateBlogReqDto.builder()
-                .blogId(UUID.fromString("48f99c85-ed6b-46c2-8f47-66f9f67040bc")) // 사용자 블로그 id
                 .blogName("Hani Tech World") // 사용자 블로그 이름
                 .blogNickname("hanitech") // 사용자 블로그 별명. 닉네임
                 .blogIntro("Hani Tech World는 최신 기술 정보와 실용적인 IT 팁을 제공하는 블로그입니다.") // 사용자 블로그 한 줄 소개
+                .blogProfileImg("http://haniTechProfileImageUrl.com") // 사용자 프로필 이미지
                 .build();
 
-        blogService.createBlog(reqDto);
+        blogService.createBlog(blogId, reqDto);
 
         // when: 블로그를 생성하려고 하면
         PostCreateBlogReqDto newReqDto = PostCreateBlogReqDto.builder()
-                .blogId(UUID.fromString("48f99c85-ed6b-46c2-8f47-66f9f67040bc")) // 사용자 블로그 id
                 .blogName("Wooseok Tech World") // 사용자 블로그 이름
                 .blogNickname("wooseoktech") // 사용자 블로그 별명. 닉네임
                 .blogIntro("Woo Seok Tech World는 최신 기술 정보와 실용적인 IT 팁을 제공하는 블로그입니다.") // 사용자 블로그 한 줄 소개
+                .blogProfileImg("http://wooseokTechProfileImageUrl.com") // 사용자 프로필 이미지
                 .build();
 
         // then: ALREADY_BLOG_EXISTS_EXCEPTION 예외가 발생해야 한다.
         BaseException thrownException = assertThrows(BaseException.class, () -> {
-            blogService.createBlog(newReqDto);
+            blogService.createBlog(blogId, newReqDto);
         });
 
         assertEquals(BaseErrorCode.ALREADY_BLOG_EXISTS_EXCEPTION, thrownException.getErrorCode());
@@ -110,25 +114,27 @@ class BlogServiceUnitTest {
     @Test
     void 블로그_생성_실패_블로그_닉네임_중복() throws BaseException {
         // given: 사전에 있는 블로그 닉네임으로
+        UUID blogId = UUID.fromString("48f99c85-ed6b-46c2-8f47-66f9f67040bc");
         PostCreateBlogReqDto reqDto = PostCreateBlogReqDto.builder()
-                .blogId(UUID.fromString("48f99c85-ed6b-46c2-8f47-66f9f67040bc")) // 사용자 블로그 id
                 .blogName("Hani Tech World") // 사용자 블로그 이름
                 .blogNickname("hanitech") // 사용자 블로그 별명. 닉네임
                 .blogIntro("Hani Tech World는 최신 기술 정보와 실용적인 IT 팁을 제공하는 블로그입니다.") // 사용자 블로그 한 줄 소개
+                .blogProfileImg("http://haniTechProfileImageUrl.com") // 사용자 프로필 이미지
                 .build();
 
-        blogService.createBlog(reqDto);
+        blogService.createBlog(blogId, reqDto);
 
         // when: 블로그를 생성하려고 하면
+        UUID anotherBlogId = UUID.fromString("5a7a63d7-02c7-43a0-82ec-f2a80bd2eba4");
         PostCreateBlogReqDto newReqDto = PostCreateBlogReqDto.builder()
-                .blogId(UUID.fromString("5a7a63d7-02c7-43a0-82ec-f2a80bd2eba4")) // 사용자 블로그 id
                 .blogName("Wooseok Tech World") // 사용자 블로그 이름
                 .blogNickname("hanitech") // 사용자 블로그 별명. 닉네임
                 .blogIntro("Woo Seok Tech World는 최신 기술 정보와 실용적인 IT 팁을 제공하는 블로그입니다.") // 사용자 블로그 한 줄 소개
+                .blogProfileImg("http://wooseokTechProfileImageUrl.com") // 사용자 프로필 이미지
                 .build();
 
         // then: ALREADY_BLOG_NICKNAME_EXISTS_EXCEPTION 예외가 발생해야 한다.
-        BaseException thrownException = assertThrows(BaseException.class, () -> blogService.createBlog(newReqDto));
+        BaseException thrownException = assertThrows(BaseException.class, () -> blogService.createBlog(anotherBlogId, newReqDto));
 
         assertEquals(BaseErrorCode.ALREADY_BLOG_NICKNAME_EXISTS_EXCEPTION, thrownException.getErrorCode());
     }
