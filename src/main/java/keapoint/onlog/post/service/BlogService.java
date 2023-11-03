df25c08ea77d5bc818552905fe73d68108c283f5
SeungHyeon <namsh1125@naver.com>
2023-11-03 16:20:38 +0900

    [Update] 블로그 생성 요청 API 수정

--- src/main/java/keapoint/onlog/post/service/BlogService.java
+++ src/main/java/keapoint/onlog/post/service/BlogService.java
@@ -24,13 +24,13 @@ public class BlogService {
     private final FollowRepository followRepository;
 
     @Transactional
-    public BlogDto createBlog(UUID blogId, PostCreateBlogReqDto data) throws BaseException {
+    public BlogDto createBlog(PostCreateBlogReqDto data) throws BaseException {
         try {
             // 로깅
             log.info("블로그 생성 요청 정보: " + data.toString());
 
             // 해당 id를 기반으로 이미 블로그가 있는 경우 예외를 터트린다.
-            if (blogRepository.findById(blogId).isPresent())
+            if (blogRepository.findById(data.getBlogId()).isPresent())
                 throw new BaseException(BaseErrorCode.ALREADY_BLOG_EXISTS_EXCEPTION);
 
             // 해당 nickname의 블로그가 있는 경우 예외를 터트린다.
@@ -38,7 +38,7 @@ public class BlogService {
                 throw new BaseException(BaseErrorCode.ALREADY_BLOG_NICKNAME_EXISTS_EXCEPTION);
 
             // 예외가 없는 경우 블로그를 생성한다.
-            Blog blog = blogRepository.save(data.toEntity(blogId));
+            Blog blog = blogRepository.save(data.toEntity(data.getBlogId()));
             log.info("생성된 블로그 정보: " + blog);
 
             // 생성 결과를 반환한다.
