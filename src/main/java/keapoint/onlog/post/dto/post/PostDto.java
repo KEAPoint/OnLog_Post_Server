15dbe67441d1c784655e2b9261c5620cda08ac2d
SeungHyeon <namsh1125@naver.com>
2023-11-06 14:14:04 +0900

    [Update] PostDto 응답 data 변경

--- src/main/java/keapoint/onlog/post/dto/post/PostDto.java
+++ src/main/java/keapoint/onlog/post/dto/post/PostDto.java
@@ -20,11 +20,13 @@ public class PostDto {
     private String content; // 게시글 내용
     private String summary; // 게시글 3줄 요약
     private String thumbnailLink; // 게시글 thumbnail 사진 위치
+    private Long likesCount; // 게시글 좋아요 갯수
     private Boolean isPublic; // 게시글 공개 여부
     private Boolean modified; // 게시글 수정 여부
     private TopicDto topic; // 게시글 주제
     private CategoryDto category; // 게시글 카테고리
     private List<HashtagDto> hashtagList; // 해시태그 리스트
+    private int commentsCounts; // 게시글 댓글 갯수
     private List<CommentDto> comments; // 게시글 댓글
     private BlogDto writer; // 작성자
     private LocalDateTime createdAt; // Row 생성 시점
@@ -36,6 +38,7 @@ public class PostDto {
         this.content = post.getContent();
         this.summary = post.getSummary();
         this.thumbnailLink = post.getThumbnailLink();
+        this.likesCount = post.getLikesCount();
         this.isPublic = post.getIsPublic();
         this.modified = post.getModified();
         this.topic = new TopicDto(post.getTopic());
@@ -44,6 +47,7 @@ public class PostDto {
                 .map(HashtagDto::new)
                 .toList();
 
+        this.commentsCounts = post.getComments().size();
         this.comments = post.getComments().stream()
                 .map(CommentDto::new)
                 .toList();
