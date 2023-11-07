60b9a9d10dc52c3ca8a439cce1566ef5a36baca5
namsh1125 <namsh1125@naver.com>
2023-11-08 00:00:37 +0900

    [Fix] 댓글의 그룹을 조회하는 쿼리문 변경 - findMaxRefByPost

--- src/main/java/keapoint/onlog/post/repository/CommentRepository.java
+++ src/main/java/keapoint/onlog/post/repository/CommentRepository.java
@@ -3,6 +3,7 @@ package keapoint.onlog.post.repository;
 import keapoint.onlog.post.entity.Comment;
 import keapoint.onlog.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +12,7 @@ import java.util.UUID;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
+    @Query("select max(c.ref) from Comment c where c.post =?1")
     Optional<Long> findMaxRefByPost(Post post);
 
     @Query("select sum(answerNum) from Comment where ref = ?1")
@@ -19,6 +21,7 @@ public interface CommentRepository extends JpaRepository<Comment, UUID> {
     @Query("select max(step) from Comment where ref = ?1")
     Long getMaxStep(Long ref);
 
+    @Modifying
     @Query("update Comment set refOrder = refOrder + 1 where ref = ?1 and refOrder > ?2")
     void updateRefOrder(Long ref, Long refOrder);
 }
