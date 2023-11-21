c497a7811aa8fd62649378bef18a7474dbe76c71
SeungHyeon <namsh1125@naver.com>
2023-11-22 00:17:41 +0900

    [Refactor] 특정 게시글 조회 API 튜닝 - 삭제된 댓글의 좋아요. 정보는 DB에서 가져오지 않게 수정 - stream 최적화 (한 번만 사용) - 로그 메시지 최적화

--- src/main/java/keapoint/onlog/post/repository/UserCommentLikeRepository.java
+++ src/main/java/keapoint/onlog/post/repository/UserCommentLikeRepository.java
@@ -4,6 +4,8 @@ import keapoint.onlog.post.entity.Blog;
 import keapoint.onlog.post.entity.Comment;
 import keapoint.onlog.post.entity.UserCommentLike;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,7 +15,16 @@ import java.util.Optional;
 public interface UserCommentLikeRepository extends JpaRepository<UserCommentLike, Long> {
     Optional<UserCommentLike> findByBlogAndComment(Blog blog, Comment comment);
 
-    List<UserCommentLike> findByBlogAndCommentIn(Blog blog, List<Comment> comments);
+    /**
+     * 블로그와 유효한(삭제되지 않은) 댓글 목록에 해당하는 좋아요 정보를 조회한다.
+     *
+     * @param blog     좋아요 정보를 조회할 블로그
+     * @param comments 좋아요 정보를 조회할 댓글 목록
+     * @return 블로그와 유효한 댓글 목록에 해당하는 좋아요 정보 리스트
+     */
+    @Query("SELECT u FROM UserCommentLike u WHERE u.blog = :blog AND u.comment IN :comments AND u.comment.status = true")
+    List<UserCommentLike> findByBlogAndValidComments(@Param("blog") Blog blog, @Param("comments") List<Comment> comments);
 
     void deleteByComment(Comment comment);
 }
+
